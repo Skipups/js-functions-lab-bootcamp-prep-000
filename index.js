dad0cde26256fe3775c803a59572efcee9729062
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-03-25 00:48:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays(){
   console.log("Happy holidays!")
 }
-function happyHolidaysTo(holiday, name){
+function happyHolidaysTo('holiday', 'name'){
 return `Happy ${holiday}, ${name}!`
 }
